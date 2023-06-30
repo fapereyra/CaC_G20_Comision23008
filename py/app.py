c25893e7a6561aa2b9ca51ceb1929189b7735919
Francisco Antonio Pereyra <fpereyra@fi.uba.ar>
2023-06-30 20:31:29 -0300

    app terminada, falta la funcion de mostrar y acomodar unos datos

--- py/app.py
+++ py/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask,  jsonify, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 
@@ -20,22 +20,14 @@ def create_table():
             dni int (8) NOT NULL,
             nombreyapellido varchar (45) NOT NULL,
             sexo varchar (10) NOT NULL,
-            fechanacimiento date NOT NULL,
+            categoria varchar (10) NOT NULL,
             PRIMARY KEY (dni)
         )
     ''')
-    # deporte varchar (30)
-    # natalacio date NOT NULL,
-    # edad int(3) NOT NULL,
-    # telefono int(12) NOT NULL,
-    # email varchar (30) NOT NULL,
-    # contrasenia varchar (30) NOT NULL,
-
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS deportes (
             id_d int (4) NOT NULL,
             nombre varchar (30) NOT NULL,
-            categoria int (4) NOT NULL,
             arancel int (5) NOT NULL,
             PRIMARY KEY (`id_d`)
         )
@@ -50,17 +42,6 @@ def create_table():
             FOREIGN KEY (`id_deporte`) REFERENCES deportes (`id_d`)
         )
     ''')
-
-    # cursor.execute('''
-    #     CREATE TABLE IF NOT EXISTS actividades (
-    #         id_a int (4) NOT NULL,
-    #         inscriptos int (4) NOT NULL,
-    #         deporte int (4) NOT NULL,
-    #         PRIMARY KEY (id_a),
-    #         FOREIGN KEY (inscriptos) REFERENCES socios (id_s),
-    #         FOREIGN KEY (deporte) REFERENCES deportes (id_d)
-    #     )
-    # ''')
     conn.commit()
     cursor.close()
     conn.close()
@@ -79,43 +60,58 @@ create_database()
 # Definimos la clase "Socio"
 # -------------------------------------------------------------------
 class Socio:
-    def __init__(self, dni, nombre, apellido):
+    def __init__(self, dni, nombreyapellido, sexo, fechanacimiento):
         self.dni = dni
-        self.nombre = nombre
-        self.apellido = apellido
-#        self.deporte = deporte
-#        self.contrasenia = contrasenia
-#        self.edad = edad
-#        self.natalicio = natalicio
-#        self.telefono = telefono
-#        self.email = email
+        self.nombreyapellido = nombreyapellido
+        self.sexo = sexo
+        self.fechanacimiento = fechanacimiento
 
-    def modificar(self, n_nombre, n_apellido):
-        self.nombre = n_nombre
-        self.apellido = n_apellido
-#        self.deporte = n_deporte
-#        self.contrasenia = n_contrasenia
-#        self.edad = n_edad
-#        self.natalicio = n_natalicio
-#        self.telefono = n_telefono
-#        self.email = n_email
+    def modificar(self, n_nombreyapellido, n_sexo, n_fechanacimiento):
+        self.nombreyapellido = n_nombreyapellido
+        self.sexo = n_sexo
+        self.fechanacimiento = n_fechanacimiento
+
+# -------------------------------------------------------------------
+# Definimos la clase "Deportes"
+# -------------------------------------------------------------------
+class Deportes: #-- CREO QUE FALTA CONECTAR CON BASE DE DATOS --#
+    def __init__(self, id_d, nombre, categoria, arancel):
+        self.id_d = id_d
+        self.nombre = nombre
+        self.categoria = categoria
+        self.arancel = arancel
+
+    def consultar_deporte(self, id_d):
+        self.cursor.execute("SELECT * FROM deportes WHERE id_d = ?", (id_d,))
+        row = self.cursor.fetchone()
+        if row:
+            id_d, nombre, categoria, arancel = row
+            return Deportes(id_d, nombre, categoria, arancel)
+        return None
+
+# -------------------------------------------------------------------
+# Definimos la clase "Inscripciones"
+# -------------------------------------------------------------------
+class Inscripcion:
+    def __init__(self, dni_socio, id_d):
+        self.dni_socio = dni_socio
+        self.id_d = id_d
 
 
 # -------------------------------------------------------------------
 # Definimos la clase "Administracion"
 # -------------------------------------------------------------------
-class Administracion:
+class AdministracionDeSocios:
     def __init__(self):
         self.conexion = get_db_connection()
         self.cursor = self.conexion.cursor()
 
-    def dar_alta_socio(self, dni, nombre, apellido, deporte):
+    def dar_alta_socio(self, dni, nombreyapellido, sexo, fechanacimiento):
         socio_existente = self.consultar_socio(dni)
         if socio_existente:
             return jsonify({'message': 'Ya existe un socio con ese DNI.'}), 400
 
-        # nuevo_socio = Socio(dni, nombre, apellido, deporte)
-        self.cursor.execute("INSERT INTO socios VALUES (?, ?, ?, ?)", (dni, nombre, apellido, deporte))
+        self.cursor.execute("INSERT INTO socios VALUES (?, ?, ?, ?)", (dni, nombreyapellido, sexo, fechanacimiento))
         self.conexion.commit()
         return jsonify({'message': 'Socio dado de alta correctamente.'}), 200
 
@@ -123,16 +119,16 @@ class Administracion:
         self.cursor.execute("SELECT * FROM socios WHERE dni = ?", (dni,))
         row = self.cursor.fetchone()
         if row:
-            dni, nombre, apellido, deporte = row
-            return Socio(dni, nombre, apellido, deporte)
+            dni, nombreyapellido, sexo, fechanacimiento = row
+            return Socio(dni, nombreyapellido, sexo, fechanacimiento)
         return None
 
-    def actualizar_socio(self, dni, nombre, apellido, deporte):
+    def actualizar_socio(self, dni, nombreyapellido, sexo, fechanacimiento):
         socio = self.consultar_socio(dni)
         if socio:
-            socio.modificar(nombre, apellido, deporte)
-            self.cursor.execute("UPDATE socios SET nombre = ?, apellido = ?, deporte = ? WHERE dni = ?",
-                                (nombre, apellido, deporte, dni))
+            socio.modificar(nombreyapellido, sexo, fechanacimiento)
+            self.cursor.execute("UPDATE socios SET nombreyapellido = ?, sexo = ?, fechanacimiento = ? WHERE dni = ?",
+                                (nombreyapellido, sexo, fechanacimiento, dni))
             self.conexion.commit()
             return jsonify({'message': 'Socio modificado correctamente.'}), 200
         return jsonify({'message': 'Socio no encontrado.'}), 404
@@ -142,12 +138,14 @@ class Administracion:
         rows = self.cursor.fetchall()
         socios = []
         for row in rows:
-            dni, nombre, apellido, deporte = row
-            producto = {'dni': dni, 'nombre': nombre, 'apellido': apellido, 'deporte': deporte}
+            dni, nombreyapellido, sexo, fechanacimiento = row
+            producto = {'dni': dni, 'nombreyapellido': nombreyapellido, 'sexo': sexo, 'fechanacimiento': fechanacimiento}
             socios.append(producto)
         return jsonify(socios), 200
 
     def dar_baja_socio(self, dni):
+        InscripcionADeporte.desincribir_todos_los_deportes(dni)
+
         self.cursor.execute("DELETE FROM socios WHERE dni = ?", (dni,))
         if self.cursor.rowcount > 0:
             self.conexion.commit()
@@ -158,61 +156,64 @@ class Administracion:
 # -------------------------------------------------------------------
 # Definimos la clase "Inscripcion"
 # -------------------------------------------------------------------
-class Inscripcion:
+class InscripcionADeporte:
     def __init__(self):
         self.conexion = get_db_connection()
         self.cursor = self.conexion.cursor()
-        self.inscriptos = [] # habria que sacar esta lista y acomodar todo para que funcione con la tabla inscripciones
 
-    def inscribir(self, dni, deporte, administracion):
-        socio = administracion.consultar_socio(dni)
-        if dni is None:
+    def consultar_inscripcion(self, dni, id_d):
+        self.cursor.execute("SELECT * FROM inscripciones WHERE dni_socio = ? AND id_deporte = ?", (dni, id_d))
+        row = self.cursor.fetchone()
+        if row:
+            dni_socio, id_d = row
+            return Inscripcion(dni_socio, id_d)
+        return None
+
+    def inscribir(self, dni, id_d, id):
+        socio = AdministracionDeSocios.consultar_socio(dni)
+        if socio is None:
             return jsonify({'message': 'El socio no existe.'}), 404
-        if socio.deporte >= 1: #ESTO HAY QUE IMPLEMENTARLO MEJOR PARA QUE PUEDA TENER ALGUNOS DEPORTES DIFERENTES
-            return jsonify({'message': 'Solo se puede agregar 1.'}), 400
 
-        self.cursor.execute("SELECT dni_deporte FROM inscripciones WHERE dni_socio = ?", (dni,))
-        row = self.cursor.fetchall()
-        # incompleto
+        inscripcion = InscripcionADeporte.consultar_inscripcion(id, dni, id_d)
+        if inscripcion is None:
+            self.cursor.execute("INSERT INTO inscripciones VALUES (?, ?, ?)", (id, dni, id_d))
+            self.conexion.commit()
+            return jsonify({'message': 'Socio inscripto correctamente.'}), 200
+        return jsonify({'message': 'El socio ya se encuentra inscripto en este deporte'}), 404
 
-        for inscripto in self.inscriptos:
-            if inscripto.dni == dni:
-                inscripto.deporte += deporte
-                self.cursor.execute("UPDATE socios SET deporte = deporte - ? WHERE dni = ?",
-                                    (deporte, dni))
-                self.conexion.commit()
-                return jsonify({'message': 'Socio inscripto correctamente.'}), 200
+    def desinscribir(self, dni, id_d):
+        self.cursor.execute("DELETE FROM inscripciones WHERE dni = ? AND id_deporte = ?", (dni, id_d))
+        if self.cursor.rowcount > 0:
+            self.conexion.commit()
+            return jsonify({'message': 'Socio desinscripto correctamente.'}), 200
+        return jsonify({'message': 'Inscripcion no encontrada.'}), 404
 
-        #CODIGO PARA AGREGAR UN NUEVO DEPORTE (SI QUISIERAMOS) 
-        #nuevo_item = Producto(codigo, producto.descripcion, cantidad, producto.precio)
-        #self.items.append(nuevo_item)
-        #self.cursor.execute("UPDATE productos SET cantidad = cantidad - ? WHERE codigo = ?",
-        #                    (cantidad, codigo))
-        #self.conexion.commit()
-        #return jsonify({'message': 'Producto agregado al carrito correctamente.'}), 200
-
-    def quitar(self, dni, deporte):
-        for inscripto in self.inscriptos:
-            if inscripto.dni == dni:
-                if deporte > inscripto.deporte:
-                    return jsonify({'message': 'Tiene que tener al menos una inscripción a un deporte, no puede no tener ninguna.'}), 400
-                inscripto.deporte -= deporte
-                if inscripto.deporte == 0:
-                    self.inscriptos.remove(inscripto)
-                self.cursor.execute("UPDATE socio SET deporte = deporte + ? WHERE dni = ?",
-                                    (deporte, dni))
-                self.conexion.commit()
-                return jsonify({'message': 'Socio sin inscripciones.'}), 200
-
-        return jsonify({'message': 'El socio no se ha inscripto a ningun deporte.'}), 404
+    def desincribir_todos_los_deportes(self, dni):
+        self.cursor.execute("DELETE FROM inscripciones WHERE dni = ?", (dni,))
+        if self.cursor.rowcount > 0:
+            self.conexion.commit()
+            return jsonify({'message': 'Socio desinscripto correctamente.'}), 200
+        return jsonify({'message': 'Inscripcion no encontrada.'}), 404
 
     def mostrar(self):
-        socio_inscripto = []
-        for inscripto in self.inscriptos:
-            socio = {'dni': inscripto.dni, 'nombre': inscripto.nombre, 'deporte': inscripto.deporte,}
-                        #'arancel': inscripto.arancel} #COMPLETAR LUEGO CON ESE CAMPO
-            socio_inscripto.append(socio)
-        return jsonify(socio_inscripto), 200
+        self.cursor.execute("SELECT * FROM inscripciones")
+        inscripciones_procesadas = {}
+        inscripciones = self.cursor.fetchall()
+        for inscripcion in inscripciones:
+            id, dni_socio, id_deporte = inscripcion
+
+            self.cursor.execute("SELECT * FROM socios WHERE dni = ?", (dni_socio,))
+            dni, nombreyapellido, sexo, categoria = self.cursor.fetchone()
+
+            self.cursor.execute("SELECT * FROM deportes WHERE id_d = ?", (id_deporte,))
+            id_d, nombre, arancel = self.cursor.fetchone()
+
+            # INCOMPLETO
+
+            # socio = {'dni': inscripto.dni, 'nombre': inscripto.nombre, 'deporte': inscripto.deporte,}
+            #             #'arancel': inscripto.arancel} #COMPLETAR LUEGO CON ESE CAMPO
+            # socio_inscripto.append(socio)
+        return jsonify(inscripciones_procesadas), 200
 
 
 # -------------------------------------------------------------------
@@ -222,19 +223,19 @@ class Inscripcion:
 app = Flask(__name__)
 CORS(app)
 
-inscripcion = Inscripcion()         # Instanciamos una inscripcion
-administracion = Administracion()        # Instanciamos una administracion
+inscripcion = InscripcionADeporte()         # Instanciamos una inscripcion
+administracion = AdministracionDeSocios()   # Instanciamos una administracion
 
 # Ruta para obtener los datos de un socio según su DNI
 @app.route('/socios/<int:dni>', methods=['GET'])
 def obtener_socio(dni):
-    socio = administracion.consultar_producto(dni)
+    socio = administracion.consultar_socio(dni)
     if socio:
         return jsonify({
             'dni': socio.dni,
-            'nombre': socio.nombre,
-            'apellido': socio.apellido,
-            'deporte': socio.deporte
+            'nombreyapellido': socio.nombreyapellido,
+            'sexo': socio.sexo,
+            'fechanacimiento': socio.fechanacimiento
         }), 200
     return jsonify({'message': 'Socio no encontrado.'}), 404
 
@@ -243,22 +244,22 @@ def obtener_socio(dni):
 def obtener_socios():
     return administracion.listar_socios()
 
-# Ruta para agregar dar de alta un socio
+# Ruta para dar de alta un socio
 @app.route('/socios', methods=['POST'])
 def dar_alta_socio():
     dni = request.json.get('dni')
-    nombre = request.json.get('nombre')
-    apellido = request.json.get('apellido')
-    deporte = request.json.get('deporte')
-    return administracion.dar_alta_socio(dni, nombre, apellido, deporte)
+    nombreyapellido = request.json.get('nombreyapellido')
+    sexo = request.json.get('sexo')
+    fechanacimiento = request.json.get('fechanacimieno')
+    return administracion.dar_alta_socio(dni, nombreyapellido, sexo, fechanacimiento)
 
-# Ruta para modificar la informacion de un socio
-@app.route('/productos/<int:codigo>', methods=['PUT'])
-def modificar_producto(dni):
-    nuevo_nombre = request.json.get('nombre')
-    nuevo_apellido = request.json.get('apellido')
-    nuevo_deporte = request.json.get('deporte')
-    return administracion.modificar_producto(dni, nuevo_nombre, nuevo_apellido, nuevo_deporte)
+# Ruta para modificar la informacion de un socio -- CREO QUE ESTO QUEDARÍA SIN EFECTO --
+@app.route('/socios/<int:codigo>', methods=['PUT'])
+def modificar_socio(dni):
+    nuevo_nombreyapellido = request.json.get('nombreyapellido')
+    nuevo_sexo = request.json.get('sexo')
+    nuevo_fechanacimiento = request.json.get('fechanacimiento')
+    return administracion.actualizar_socio(dni, nuevo_nombreyapellido, nuevo_sexo, nuevo_fechanacimiento)
 
 # Ruta para dar de baja un socio
 @app.route('/socios/<int:dni>', methods=['DELETE'])
@@ -266,30 +267,29 @@ def dar_baja_socio(dni):
     return administracion.dar_baja_socio(dni)
 
 # Ruta para agregar inscribir un socio a un deporte
-@app.route('/inscripcion', methods=['POST'])
+@app.route('/inscripciones', methods=['POST'])
 def agregar_inscripcion():
-    dni = request.json.get('dni')
-    deporte = request.json.get('deporte')
-    inscripto = Administracion()
-    return inscripcion.agregar(dni, deporte, inscripto)
+    id = request.json.get('id')
+    dni_socio = request.json.get('dni_socio')
+    id_d = request.json.get('id_d')
+    return inscripcion.inscribir(dni_socio, id_d, id)
 
 # Ruta para dar de baja un socio de un deporte
-@app.route('/inscripcion', methods=['DELETE'])
+@app.route('/inscripciones', methods=['DELETE'])
 def quitar_inscripcion():
-    dni = request.json.get('dni')
-    deporte = request.json.get('deporte')
-    inscripto = Administracion()
-    return inscripcion.quitar(dni, deporte, inscripto, inscripcion)
+    dni_socio = request.json.get('dni_socio')
+    id_d = request.json.get('id_d')
+    return inscripcion.desinscribir(dni_socio, id_d)
 
-# Ruta para obtener el contenido de la administracion
-@app.route('/inscripcion', methods=['GET'])
-def obtener_inscripcion():
+# Ruta para obtener el contenido de las incripciones
+@app.route('/inscripciones', methods=['GET'])
+def obtener_inscripciones():
     return inscripcion.mostrar()
 
 # Ruta para obtener la lista de socios de la administracion
-@app.route('/')
-def index():
-    return 'API de Administracion'
+@app.route('/socios')
+def obtener_socios():
+    return administracion.listar_socios()
 
 # Finalmente, si estamos ejecutando este archivo, lanzamos app.
 if __name__ == '__main__':
@@ -297,8 +297,18 @@ if __name__ == '__main__':
 
 
 # INSERT INTO tabla_destino (columna1, columna2, columna3, ...)
+#-- ESTO DEBERÍA IR EN ALGUNA PARTE ESPECIFICA DEL CODIGO PARA QUE CUANDO VAYA A BUSCAR LOS DATOS EN LAS TABLAS
+#   PARA PEGARLOS EN inscripciones, EFECTIVAMENTE HAYA DATOS PARA RECUPERAR.  --#
+
+# conn = conectar()
+# cursor = conn.cursor()
+# cursor.execute("""INSERT INTO inscripciones
+#                (dni_socio, id_d)
+#                VALUES((?), (SELECT dni_socio FROM socios), (SELECT id_d FROM deportes))""")
+# conn.commit()
+# cursor.close()
+# conn.close()
+
 # SELECT columna1, columna2, columna3, ...
 # FROM tabla_origen
 # WHERE condicion;
-
-
