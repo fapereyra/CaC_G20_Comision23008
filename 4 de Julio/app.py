e6b9420de181b0f7b4277bc041acd5a6f8eeb76f
FabriProgram <130379868+FabriProgram@users.noreply.github.com>
2023-07-04 21:17:57 -0300

    Add files via upload

---
+++ 4 de Julio/app.py
@@ -0,0 +1,101 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from tablas import create_database
+from objetos import InscripcionADeporte, AdministracionDeSocios
+
+# Crear la base de datos y la/s tabla/s si no existen
+create_database()
+
+# -------------------------------------------------------------------
+# Configuración y rutas de la API Flask
+# -------------------------------------------------------------------
+
+app = Flask(__name__)
+CORS(app)
+
+inscripcion = InscripcionADeporte()         # Instanciamos una inscripcion
+administracion = AdministracionDeSocios()   # Instanciamos una administracion
+
+# Ruta para obtener los datos de un socio según su DNI
+@app.route('/socios/<int:dni>', methods=['GET'])
+def obtener_socio(dni):
+    socio = administracion.consultar_socio(dni)
+    if socio:
+        return jsonify({
+            'dni': socio.dni,
+            'nomyape': socio.nomyape,
+            'sexo': socio.sexo,
+            'categoria': socio.categoria,
+            'email': socio.email,
+            'telefono': socio.tel,
+            'direccion': socio.direccylocalidad
+        }), 200
+    return jsonify({'message': 'Socio no encontrado.'}), 404
+
+# Ruta para obtener la lista de socios dados de alta
+@app.route('/socios', methods=['GET'])
+def obtener_socios():
+    return administracion.listar_socios()
+
+# Ruta para dar de alta un socio
+@app.route('/socios', methods=['POST'])
+def dar_alta_socio():
+    dni = request.json.get('dni')
+    nomyape = request.json.get('nomyape')
+    sexo = request.json.get('sexo')
+    categoria = request.json.get('categoria')
+    email = request.json.get('email')
+    tel = request.json.get('tel')
+    direccylocalidad = request.json.get('direccylocalidad')
+    return administracion.dar_alta_socio(dni, nomyape, sexo, categoria, email, tel, direccylocalidad)
+
+# Ruta para modificar la informacion de un socio -- CREO QUE ESTO QUEDARÍA SIN EFECTO --
+@app.route('/socios/<int:codigo>', methods=['PUT'])
+def modificar_socio(dni):
+    nuevo_nomyape = request.json.get('nomyape')
+    nuevo_sexo = request.json.get('sexo')
+    nueva_categoria = request.json.get('categoria')
+    nuevo_email = request.json.get('email')
+    nuevo_tel = request.json.get('tel')
+    nuevo_direccylocalidad = request.json.get('direccylocalidad')
+    return administracion.actualizar_socio(dni, nuevo_nomyape, nuevo_sexo, nueva_categoria, nuevo_email, nuevo_tel, nuevo_direccylocalidad)
+
+# Ruta para dar de baja un socio
+@app.route('/socios/<int:dni>', methods=['DELETE'])
+def dar_baja_socio(dni):
+    return administracion.dar_baja_socio(dni)
+
+# Ruta para agregar inscribir un socio a un deporte
+@app.route('/inscripciones', methods=['POST'])
+def agregar_inscripcion():
+    id = request.json.get('id')
+    dni_socio = request.json.get('dni_socio')
+    id_d = request.json.get('id_d')
+    return inscripcion.inscribir(dni_socio, id_d, id)
+
+# Ruta para dar de baja un socio de un deporte
+@app.route('/inscripciones', methods=['DELETE'])
+def quitar_inscripcion():
+    dni_socio = request.json.get('dni_socio')
+    id_d = request.json.get('id_d')
+    return inscripcion.desinscribir(dni_socio, id_d)
+
+# Ruta para obtener las inscripciones de un socio
+@app.route('/inscripciones/<int:dni>', methods=['GET'])
+def obtener_inscripciones_socio():
+    dni_socio = request.json.get('dni_socio')
+    return inscripcion.mostrar_inscripciones_socio(dni_socio)
+
+# Ruta para obtener todas las incripciones
+@app.route('/inscripciones', methods=['GET'])
+def obtener_inscripciones():
+    return inscripcion.mostrar_todas_inscripciones()
+
+# Ruta para obtener la lista de socios de la administracion
+@app.route('/socios')
+def obtener_socios():
+    return administracion.listar_socios()
+
+# Finalmente, si estamos ejecutando este archivo, lanzamos app.
+if __name__ == '__main__':
+    app.run()
