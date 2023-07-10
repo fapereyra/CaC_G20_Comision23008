9b03b7da60624f21c70d9e8e5b2fff56745050ce
NMS47 <nicolas.salvadores93@gmail.com>
2023-07-10 18:35:59 -0500

    intentando unir con la ultima actualizacion de fabri

--- app.py
+++ app.py
@@ -4,221 +4,223 @@ from flask import Flask, jsonify, request, render_template, url_for, redirect
 from flask_cors import CORS
 from formulario import FormularioInscripcion
 from flask_bootstrap import Bootstrap4
+from tablas import create_database
+from objetos import InscripcionADeporte, AdministracionDeSocios
 
-# Configurar la conexión a la base de datos SQLite
-DATABASE = 'administracion.db'
+# # Configurar la conexión a la base de datos SQLite
+# DATABASE = 'administracion.db'
 
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE, check_same_thread=False)
-    conn.row_factory = sqlite3.Row
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect(DATABASE, check_same_thread=False)
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
-# Crear la/s tabla/s si no existe/n
-def create_table():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS socios (
-            dni int (8) NOT NULL,
-            nombreyapellido varchar (45) NOT NULL,
-            sexo varchar (10) NOT NULL,
-            categoria varchar (10) NOT NULL,
-            PRIMARY KEY (dni)
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS deportes (
-            id_d int (4) NOT NULL,
-            nombre varchar (30) NOT NULL,
-            arancel int (5) NOT NULL,
-            PRIMARY KEY (`id_d`)
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS inscripciones (
-            id int (4) NOT NULL,
-            dni_socio int (4) NOT NULL,
-            id_deporte int (4) NOT NULL,
-            PRIMARY KEY (`id`),
-            FOREIGN KEY (`dni_socio`) REFERENCES socios (`dni`),
-            FOREIGN KEY (`id_deporte`) REFERENCES deportes (`id_d`)
-        )
-    ''')
-    conn.commit()
-    cursor.close()
-    conn.close()
+# # Crear la/s tabla/s si no existe/n
+# def create_table():
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS socios (
+#             dni int (8) NOT NULL,
+#             nombreyapellido varchar (45) NOT NULL,
+#             sexo varchar (10) NOT NULL,
+#             categoria varchar (10) NOT NULL,
+#             PRIMARY KEY (dni)
+#         )
+#     ''')
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS deportes (
+#             id_d int (4) NOT NULL,
+#             nombre varchar (30) NOT NULL,
+#             arancel int (5) NOT NULL,
+#             PRIMARY KEY (`id_d`)
+#         )
+#     ''')
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS inscripciones (
+#             id int (4) NOT NULL,
+#             dni_socio int (4) NOT NULL,
+#             id_deporte int (4) NOT NULL,
+#             PRIMARY KEY (`id`),
+#             FOREIGN KEY (`dni_socio`) REFERENCES socios (`dni`),
+#             FOREIGN KEY (`id_deporte`) REFERENCES deportes (`id_d`)
+#         )
+#     ''')
+#     conn.commit()
+#     cursor.close()
+#     conn.close()
 
-# Verificar si la base de datos existe, si no, crearla y crear la/s tabla/s
-def create_database():
-    conn = sqlite3.connect(DATABASE, check_same_thread=False)
-    conn.close()
-    create_table()
+# # Verificar si la base de datos existe, si no, crearla y crear la/s tabla/s
+# def create_database():
+#     conn = sqlite3.connect(DATABASE, check_same_thread=False)
+#     conn.close()
+#     create_table()
 
-# Crear la base de datos y la/s tabla/s si no existen
-create_database()
-
-
-# -------------------------------------------------------------------
-# Definimos la clase "Socio"
-# -------------------------------------------------------------------
-class Socio:
-    def __init__(self, dni, nombreyapellido, sexo, fechanacimiento):
-        self.dni = dni
-        self.nombreyapellido = nombreyapellido
-        self.sexo = sexo
-        self.fechanacimiento = fechanacimiento
+# # Crear la base de datos y la/s tabla/s si no existen
+# create_database()
 
 
-    def modificar(self, n_nombreyapellido, n_sexo, n_fechanacimiento):
-        self.nombreyapellido = n_nombreyapellido
-        self.sexo = n_sexo
-        self.fechanacimiento = n_fechanacimiento
-
-# -------------------------------------------------------------------
-# Definimos la clase "Deportes"
-# -------------------------------------------------------------------
-class Deportes: #-- CREO QUE FALTA CONECTAR CON BASE DE DATOS --#
-    def __init__(self, id_d, nombre, categoria, arancel):
-        self.id_d = id_d
-        self.nombre = nombre
-        self.categoria = categoria
-        self.arancel = arancel
-
-    def consultar_deporte(self, id_d):
-        self.cursor.execute("SELECT * FROM deportes WHERE id_d = ?", (id_d,))
-        row = self.cursor.fetchone()
-        if row:
-            id_d, nombre, categoria, arancel = row
-            return Deportes(id_d, nombre, categoria, arancel)
-        return None
-
-# -------------------------------------------------------------------
-# Definimos la clase "Inscripciones"
-# -------------------------------------------------------------------
-class Inscripcion:
-    def __init__(self, dni_socio, id_d):
-        self.dni_socio = dni_socio
-        self.id_d = id_d
+# # -------------------------------------------------------------------
+# # Definimos la clase "Socio"
+# # -------------------------------------------------------------------
+# class Socio:
+#     def __init__(self, dni, nombreyapellido, sexo, fechanacimiento):
+#         self.dni = dni
+#         self.nombreyapellido = nombreyapellido
+#         self.sexo = sexo
+#         self.fechanacimiento = fechanacimiento
 
 
-# -------------------------------------------------------------------
-# Definimos la clase "Administracion"
-# -------------------------------------------------------------------
-class AdministracionDeSocios:
-    def __init__(self):
-        self.conexion = get_db_connection()
-        self.cursor = self.conexion.cursor()
+#     def modificar(self, n_nombreyapellido, n_sexo, n_fechanacimiento):
+#         self.nombreyapellido = n_nombreyapellido
+#         self.sexo = n_sexo
+#         self.fechanacimiento = n_fechanacimiento
 
-    def dar_alta_socio(self, dni, nombreyapellido, sexo, fechanacimiento):
-        socio_existente = self.consultar_socio(dni)
-        if socio_existente:
-            return jsonify({'message': 'Ya existe un socio con ese DNI.'}), 400
+# # -------------------------------------------------------------------
+# # Definimos la clase "Deportes"
+# # -------------------------------------------------------------------
+# class Deportes: #-- CREO QUE FALTA CONECTAR CON BASE DE DATOS --#
+#     def __init__(self, id_d, nombre, categoria, arancel):
+#         self.id_d = id_d
+#         self.nombre = nombre
+#         self.categoria = categoria
+#         self.arancel = arancel
 
-        self.cursor.execute("INSERT INTO socios VALUES (?, ?, ?, ?)", (dni, nombreyapellido, sexo, fechanacimiento))
-        self.conexion.commit()
-        return jsonify({'message': 'Socio dado de alta correctamente.'}), 200
+#     def consultar_deporte(self, id_d):
+#         self.cursor.execute("SELECT * FROM deportes WHERE id_d = ?", (id_d,))
+#         row = self.cursor.fetchone()
+#         if row:
+#             id_d, nombre, categoria, arancel = row
+#             return Deportes(id_d, nombre, categoria, arancel)
+#         return None
 
-    def consultar_socio(self, dni):
-        self.cursor.execute("SELECT * FROM socios WHERE dni = ?", (dni,))
-        row = self.cursor.fetchone()
-        if row:
-            dni, nombreyapellido, sexo, fechanacimiento = row
-            return Socio(dni, nombreyapellido, sexo, fechanacimiento)
-        return None
-
-    def actualizar_socio(self, dni, nombreyapellido, sexo, fechanacimiento):
-        socio = self.consultar_socio(dni)
-        if socio:
-            socio.modificar(nombreyapellido, sexo, fechanacimiento)
-            self.cursor.execute("UPDATE socios SET nombreyapellido = ?, sexo = ?, fechanacimiento = ? WHERE dni = ?",
-                                (nombreyapellido, sexo, fechanacimiento, dni))
-            self.conexion.commit()
-            return jsonify({'message': 'Socio modificado correctamente.'}), 200
-        return jsonify({'message': 'Socio no encontrado.'}), 404
-
-    def listar_socios(self):
-        self.cursor.execute("SELECT * FROM socios")
-        rows = self.cursor.fetchall()
-        socios = []
-        for row in rows:
-            dni, nombreyapellido, sexo, fechanacimiento = row
-            producto = {'dni': dni, 'nombreyapellido': nombreyapellido, 'sexo': sexo, 'fechanacimiento': fechanacimiento}
-            socios.append(producto)
-        return jsonify(socios), 200
-
-    def dar_baja_socio(self, dni):
-        # InscripcionADeporte.desincribir_todos_los_deportes(dni)
-
-        self.cursor.execute("DELETE FROM socios WHERE dni = ?", (dni,))
-        if self.cursor.rowcount > 0:
-            self.conexion.commit()
-            # return jsonify({'message': 'Socio dado de baja correctamente.'}), 200
-            return True
-        return False
-        # return jsonify({'message': 'Socio no encontrado.'}), 404
+# # -------------------------------------------------------------------
+# # Definimos la clase "Inscripciones"
+# # -------------------------------------------------------------------
+# class Inscripcion:
+#     def __init__(self, dni_socio, id_d):
+#         self.dni_socio = dni_socio
+#         self.id_d = id_d
 
 
-# -------------------------------------------------------------------
-# Definimos la clase "Inscripcion"
-# -------------------------------------------------------------------
-class InscripcionADeporte:
-    def __init__(self):
-        self.conexion = get_db_connection()
-        self.cursor = self.conexion.cursor()
+# # -------------------------------------------------------------------
+# # Definimos la clase "Administracion"
+# # -------------------------------------------------------------------
+# class AdministracionDeSocios:
+#     def __init__(self):
+#         self.conexion = get_db_connection()
+#         self.cursor = self.conexion.cursor()
 
-    def consultar_inscripcion(self, dni, id_d):
-        self.cursor.execute("SELECT * FROM inscripciones WHERE dni_socio = ? AND id_deporte = ?", (dni, id_d))
-        row = self.cursor.fetchone()
-        if row:
-            dni_socio, id_d = row
-            return Inscripcion(dni_socio, id_d)
-        return None
+#     def dar_alta_socio(self, dni, nombreyapellido, sexo, fechanacimiento):
+#         socio_existente = self.consultar_socio(dni)
+#         if socio_existente:
+#             return jsonify({'message': 'Ya existe un socio con ese DNI.'}), 400
 
-    def inscribir(self, dni, id_d, id):
-        socio = AdministracionDeSocios.consultar_socio(dni)
-        if socio is None:
-            return jsonify({'message': 'El socio no existe.'}), 404
+#         self.cursor.execute("INSERT INTO socios VALUES (?, ?, ?, ?)", (dni, nombreyapellido, sexo, fechanacimiento))
+#         self.conexion.commit()
+#         return jsonify({'message': 'Socio dado de alta correctamente.'}), 200
 
-        inscripcion = InscripcionADeporte.consultar_inscripcion(id, dni, id_d)
-        if inscripcion is None:
-            self.cursor.execute("INSERT INTO inscripciones VALUES (?, ?, ?)", (id, dni, id_d))
-            self.conexion.commit()
-            return jsonify({'message': 'Socio inscripto correctamente.'}), 200
-        return jsonify({'message': 'El socio ya se encuentra inscripto en este deporte'}), 404
+#     def consultar_socio(self, dni):
+#         self.cursor.execute("SELECT * FROM socios WHERE dni = ?", (dni,))
+#         row = self.cursor.fetchone()
+#         if row:
+#             dni, nombreyapellido, sexo, fechanacimiento = row
+#             return Socio(dni, nombreyapellido, sexo, fechanacimiento)
+#         return None
 
-    def desinscribir(self, dni, id_d):
-        self.cursor.execute("DELETE FROM inscripciones WHERE dni = ? AND id_deporte = ?", (dni, id_d))
-        if self.cursor.rowcount > 0:
-            self.conexion.commit()
-            return jsonify({'message': 'Socio desinscripto correctamente.'}), 200
-        return jsonify({'message': 'Inscripcion no encontrada.'}), 404
+#     def actualizar_socio(self, dni, nombreyapellido, sexo, fechanacimiento):
+#         socio = self.consultar_socio(dni)
+#         if socio:
+#             socio.modificar(nombreyapellido, sexo, fechanacimiento)
+#             self.cursor.execute("UPDATE socios SET nombreyapellido = ?, sexo = ?, fechanacimiento = ? WHERE dni = ?",
+#                                 (nombreyapellido, sexo, fechanacimiento, dni))
+#             self.conexion.commit()
+#             return jsonify({'message': 'Socio modificado correctamente.'}), 200
+#         return jsonify({'message': 'Socio no encontrado.'}), 404
 
-    def desincribir_todos_los_deportes(self, dni):
-        self.cursor.execute("DELETE FROM inscripciones WHERE dni = ?", (dni,))
-        if self.cursor.rowcount > 0:
-            self.conexion.commit()
-            return jsonify({'message': 'Socio desinscripto correctamente.'}), 200
-        return jsonify({'message': 'Inscripcion no encontrada.'}), 404
+#     def listar_socios(self):
+#         self.cursor.execute("SELECT * FROM socios")
+#         rows = self.cursor.fetchall()
+#         socios = []
+#         for row in rows:
+#             dni, nombreyapellido, sexo, fechanacimiento = row
+#             producto = {'dni': dni, 'nombreyapellido': nombreyapellido, 'sexo': sexo, 'fechanacimiento': fechanacimiento}
+#             socios.append(producto)
+#         return jsonify(socios), 200
 
-    def mostrar(self):
-        self.cursor.execute("SELECT * FROM inscripciones")
-        inscripciones_procesadas = {}
-        inscripciones = self.cursor.fetchall()
-        for inscripcion in inscripciones:
-            id, dni_socio, id_deporte = inscripcion
+#     def dar_baja_socio(self, dni):
+#         # InscripcionADeporte.desincribir_todos_los_deportes(dni)
 
-            self.cursor.execute("SELECT * FROM socios WHERE dni = ?", (dni_socio,))
-            dni, nombreyapellido, sexo, categoria = self.cursor.fetchone()
+#         self.cursor.execute("DELETE FROM socios WHERE dni = ?", (dni,))
+#         if self.cursor.rowcount > 0:
+#             self.conexion.commit()
+#             # return jsonify({'message': 'Socio dado de baja correctamente.'}), 200
+#             return True
+#         return False
+#         # return jsonify({'message': 'Socio no encontrado.'}), 404
 
-            self.cursor.execute("SELECT * FROM deportes WHERE id_d = ?", (id_deporte,))
-            id_d, nombre, arancel = self.cursor.fetchone()
 
-            # INCOMPLETO
+# # -------------------------------------------------------------------
+# # Definimos la clase "Inscripcion"
+# # -------------------------------------------------------------------
+# class InscripcionADeporte:
+#     def __init__(self):
+#         self.conexion = get_db_connection()
+#         self.cursor = self.conexion.cursor()
 
-            # socio = {'dni': inscripto.dni, 'nombre': inscripto.nombre, 'deporte': inscripto.deporte,}
-            #             #'arancel': inscripto.arancel} #COMPLETAR LUEGO CON ESE CAMPO
-            # socio_inscripto.append(socio)
-        return jsonify(inscripciones_procesadas), 200
+#     def consultar_inscripcion(self, dni, id_d):
+#         self.cursor.execute("SELECT * FROM inscripciones WHERE dni_socio = ? AND id_deporte = ?", (dni, id_d))
+#         row = self.cursor.fetchone()
+#         if row:
+#             dni_socio, id_d = row
+#             return Inscripcion(dni_socio, id_d)
+#         return None
+
+#     def inscribir(self, dni, id_d, id):
+#         socio = AdministracionDeSocios.consultar_socio(dni)
+#         if socio is None:
+#             return jsonify({'message': 'El socio no existe.'}), 404
+
+#         inscripcion = InscripcionADeporte.consultar_inscripcion(id, dni, id_d)
+#         if inscripcion is None:
+#             self.cursor.execute("INSERT INTO inscripciones VALUES (?, ?, ?)", (id, dni, id_d))
+#             self.conexion.commit()
+#             return jsonify({'message': 'Socio inscripto correctamente.'}), 200
+#         return jsonify({'message': 'El socio ya se encuentra inscripto en este deporte'}), 404
+
+#     def desinscribir(self, dni, id_d):
+#         self.cursor.execute("DELETE FROM inscripciones WHERE dni = ? AND id_deporte = ?", (dni, id_d))
+#         if self.cursor.rowcount > 0:
+#             self.conexion.commit()
+#             return jsonify({'message': 'Socio desinscripto correctamente.'}), 200
+#         return jsonify({'message': 'Inscripcion no encontrada.'}), 404
+
+#     def desincribir_todos_los_deportes(self, dni):
+#         self.cursor.execute("DELETE FROM inscripciones WHERE dni = ?", (dni,))
+#         if self.cursor.rowcount > 0:
+#             self.conexion.commit()
+#             return jsonify({'message': 'Socio desinscripto correctamente.'}), 200
+#         return jsonify({'message': 'Inscripcion no encontrada.'}), 404
+
+#     def mostrar(self):
+#         self.cursor.execute("SELECT * FROM inscripciones")
+#         inscripciones_procesadas = {}
+#         inscripciones = self.cursor.fetchall()
+#         for inscripcion in inscripciones:
+#             id, dni_socio, id_deporte = inscripcion
+
+#             self.cursor.execute("SELECT * FROM socios WHERE dni = ?", (dni_socio,))
+#             dni, nombreyapellido, sexo, categoria = self.cursor.fetchone()
+
+#             self.cursor.execute("SELECT * FROM deportes WHERE id_d = ?", (id_deporte,))
+#             id_d, nombre, arancel = self.cursor.fetchone()
+
+#             # INCOMPLETO
+
+#             # socio = {'dni': inscripto.dni, 'nombre': inscripto.nombre, 'deporte': inscripto.deporte,}
+#             #             #'arancel': inscripto.arancel} #COMPLETAR LUEGO CON ESE CAMPO
+#             # socio_inscripto.append(socio)
+#         return jsonify(inscripciones_procesadas), 200
 
 
 # -------------------------------------------------------------------
@@ -323,14 +325,15 @@ def formulario_socio():
     if request.method == 'POST':
         if form.validate_on_submit():
             dni = form.dni.data
-            nombreyapellido = form.nombreyapellido.data
+            nomyape = form.nombreyapellido.data
             email = form.email.data
             sexo = form.sexo.data
+            categoria = 1
             fechanacimiento = form.fechanacimiento.data
             edad = form.edad.data
-            telefono = form.telefono.data
-            print(dni, nombreyapellido, email, sexo, fechanacimiento, edad, telefono)
-            administracion.dar_alta_socio(dni, nombreyapellido, sexo, fechanacimiento)
+            tel = form.telefono.data
+            direccylocalidad = "CABA"
+            administracion.dar_alta_socio(dni, nomyape, sexo, categoria, email, tel, direccylocalidad)
             return redirect(url_for("formulario_exitoso"))
     return render_template("formulario.html", form=form)
 
